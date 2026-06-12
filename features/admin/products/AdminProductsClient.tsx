af84b9fa4db05466b301fead14d9cbb4f6c5fbce
vktrprfl <vktrprfl@gmail.com>
2026-06-12 19:12:59 +0700

    fix admin products

--- features/admin/products/AdminProductsClient.tsx
+++ features/admin/products/AdminProductsClient.tsx
@@ -1,7 +1,9 @@
 // app/admin/products/AdminProductsClient.tsx
 'use client';
 
-import AdminProductsTable from '@/features/admin/products/components/AdminProductsTable';
+
+
+import AdminProductsTable from "@/features/admin/products/products-table/AdminProductsTable";
 
 export default function AdminProductsClient() {
     return <AdminProductsTable />;
