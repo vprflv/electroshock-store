ef686f2db536a0bcb67a0ba9ec3ca374bc20e981
vktrprfl <vktrprfl@gmail.com>
2026-06-16 10:56:47 +0700

    add proxy img

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: '*.supabase.co',
             },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
         ],
         unoptimized: true,
     },
